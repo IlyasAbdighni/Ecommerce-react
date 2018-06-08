ed62861a851986ea9980f737bcf7683916a2109f
Ilyas Abdighni <udunig@hotmail.com>
2018-06-08 16:50:38 +0200

    first init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Let us begin!')
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
