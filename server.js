f3222b838265254cedd6bacd61f77e58228fce3b
Ilyas Abdighni <udunig@hotmail.com>
2018-07-02 21:06:27 +0200

    jwt is being added

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
-app.get('/', (req, res) => {
-    res.send('Let us begin!')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// log requests to console
+app.use(morgan("dev"));
+
+// Home page
+app.get("/", (req, res) => {
+	res.send("Let us begin!");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
